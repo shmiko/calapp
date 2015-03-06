46b01a2ddb2f5bfb40d27effcb1877636b3bc064
Paul Jones <shmiko@gmail.com>
2015-03-07 10:32:57 +1100

    mean goal

---
+++ server/config.js
@@ -0,0 +1,11 @@
+'use strict';
+//================================================================================
+// Module
+//================================================================================
+module.exports = {
+    google: {
+        clientId: '407717796667-1tmpnsdfg8861sco2qtrs6ahlb5k3opm.apps.googleusercontent.com',
+        clientSecret: 'UR9IJpLamrjN4vWCzn5gw4Ha',
+        callbackURL: 'http://localhost:7000/auth/google/callback'
+    }
+};
